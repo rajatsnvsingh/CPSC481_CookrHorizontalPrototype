40fec91e6aea356ef532371b1b2bf092f85ae457
Rajat Singh <rajat.singh@ucalgary.ca>
2018-10-19 12:26:07 -0600

    feat(setup basic pages, search page info injection)

---
+++ CookrHorizontalPrototype/CookrHorizontalPrototype/BasePages/Home.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CookrHorizontalPrototype.BasePages
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Search(SearchField.Text));
+        }
+
+        private void SearchField_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (SearchField.Text == "Search")
+            {
+                SearchField.Text = "";
+                SearchField.Foreground = Brushes.Black;
+            }
+        }
+
+        private void SearchField_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (SearchField.Text == "")
+            {
+                SearchField.Text = "Search";
+                SearchField.Foreground = Brushes.LightGray;
+            }
+        }
+    }
+}
---
+++ CookrHorizontalPrototype/CookrHorizontalPrototype/BasePages/Search.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CookrHorizontalPrototype.BasePages
+{
+
+    /// <summary>
+    /// Interaction logic for Search.xaml
+    /// </summary>
+    public partial class Search : Page
+    {
+        String SearchString;
+        public Search()
+        {
+            InitializeComponent();
+        }
+        public Search(String search)
+        {
+            SearchString = search;
+            InitializeComponent();
+            SearchedLabel.Content = "\"" + SearchString + "\"";
+        }
+
+        private void HomeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Home());
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Search(SearchField.Text));
+        }
+    }
+}
